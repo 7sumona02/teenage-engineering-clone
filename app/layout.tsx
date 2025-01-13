732ca9da5b95bfd0fb5968a153875b52f1b4c148
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-01-14 00:44:33 +0530

    edits

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
